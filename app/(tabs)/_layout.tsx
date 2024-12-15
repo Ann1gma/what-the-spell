729dd5d49243c29531215e7a2da352017179ba54
Ann Bacharach <ann_136@hotmail.com>
2024-12-15 22:15:39 +0100

    Refactored /(tabs)/_layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -8,86 +8,6 @@ import useAuth from "@/hooks/useAuth";
 export default function TabLayout() {
 	const { currentUser } = useAuth();
 
-	if (!currentUser) {
-		return (
-			<Tabs
-				screenOptions={{
-					tabBarActiveTintColor: "#990000",
-					tabBarInactiveTintColor: "#2B2B2B",
-					headerShown: false,
-					tabBarStyle: { height: 65 },
-					tabBarIconStyle: { margin: "auto" },
-					tabBarActiveBackgroundColor: "#F0E4D1",
-					tabBarInactiveBackgroundColor: "#F0E4D1",
-				}}
-			>
-				<Tabs.Screen
-					name="index"
-					options={{
-						title: "Spellbook",
-						tabBarIcon: ({ color }) => <FontAwesome6 name="book-bookmark" size={28} style={[{ marginBottom: -3, color: color }]} />,
-					}}
-				/>
-
-				<Tabs.Screen
-					name="Profile"
-					options={{
-						title: "Profile",
-						tabBarIcon: ({ color }) => <Ionicons name="person-circle" size={28} style={[{ marginBottom: -3, color: color }]} />,
-					}}
-				/>
-
-				<Tabs.Screen
-					name="Characters"
-					options={{
-						headerShown: false,
-						href: null,
-					}}
-				/>
-
-				<Tabs.Screen
-					name="FilterSpellbook"
-					options={{
-						headerShown: false,
-						href: null,
-					}}
-				/>
-
-				<Tabs.Screen
-					name="spells/[id]"
-					options={{
-						headerShown: false,
-						href: null,
-					}}
-				/>
-
-				<Tabs.Screen
-					name="Signup"
-					options={{
-						headerShown: false,
-						href: null,
-					}}
-				/>
-
-				<Tabs.Screen
-					name="AddCharacter"
-					options={{
-						headerShown: false,
-						href: null,
-					}}
-				/>
-
-				<Tabs.Screen
-					name="characters/[id]"
-					options={{
-						headerShown: false,
-						href: null,
-					}}
-				/>
-			</Tabs>
-		);
-	}
-
 	return (
 		<Tabs
 			screenOptions={{
@@ -110,10 +30,17 @@ export default function TabLayout() {
 
 			<Tabs.Screen
 				name="Characters"
-				options={{
-					title: "Characters",
-					tabBarIcon: ({ color }) => <FontAwesome5 name="dice-d20" size={28} style={[{ marginBottom: -3, color: color }]} />,
-				}}
+				options={
+					!currentUser
+						? {
+								headerShown: false,
+								href: null,
+						  }
+						: {
+								title: "Characters",
+								tabBarIcon: ({ color }) => <FontAwesome5 name="dice-d20" size={28} style={[{ marginBottom: -3, color: color }]} />,
+						  }
+				}
 			/>
 
 			<Tabs.Screen
